9e21e5f8c0116382ab95b0b8e7548f6b6b9eb00d
amfraas <amfraas@users.noreply.github.com>
2016-11-14 00:03:20 -0500

    Update fetchcomments.py
    Andy,
    I had to update this for Python 3 :( Also I realized I was getting the API call wrong (I needed to call the thread one not the posts) and I can use the thread identifier which is much easier than the full url. I am terrible at python though and super lazy as you can see with my 'limit' workaround. I ran into encoding issues which made me want to scream but fixed now. The nearly 3,000 comments on the piece generate about 5MB of JSON!

--- fetchcomments.py
+++ fetchcomments.py
@@ -1,42 +1,43 @@
-import urllib, json
+import urllib.request, urllib.parse, urllib.error, json
 
-disqus = "http://disqus.com/api/3.0/posts/list.json"
+disqus = "http://disqus.com/api/3.0/threads/listPosts.json"
 
 
-def makeURL(base, key, forum, thread, cursor=None):
-  url = "%s?api_key=%s&forum=%s&thread=%s" % (base, key, forum, thread)
+def makeURL(disqus, key, forum, thread, cursor=None):
+  url = "%s?api_key=%s&forum=%s&thread=%s" % (disqus, key, forum, thread)
   if cursor:
     url = "%s&cursor=%s" % (cursor)
   return url
 
 def getJSON(url):
-  resp = urllib.urlopen(url)
-  data = json.loads(resp.read())
+  resp = urllib.request.urlopen(url)
+  data = json.loads(resp.read().decode('utf-8'))
   return data
 
 def getNext(data):
-  if data and data.has_key("cursor"):
+  if data and "cursor" in data:
     cursor = data["cursor"]
-    if cursor.has_key("next"):
+    if "next" in cursor:
       return cursor["next"]
   return None
 
 
 api_key = open("api_key.txt").read().strip()
 forum = "breitbartproduction"
-thread = "link:http://www.breitbart.com/tech/2016/11/12/report-university-of-pennsylvania-offers-puppies-coloring-books-to-students-distraught-over-trump-win"
+thread = "ident:5513922&limit=100"
 
 
 url = makeURL(disqus, api_key, forum, thread)
 data = getJSON(url)
 hops = 0
-while data and hops < 3:
+
+while data and hops < 200:
   nextPage = getNext(data)
-  print nextPage
+  with open("Output.txt", "a", encoding='utf-8') as text_file:
+    print(data, file=text_file)
   if nextPage:
     url2 = "%s&cursor=%s" % (url, nextPage)
     data = getJSON(url2)
   else:
     break
   hops += 1
-
